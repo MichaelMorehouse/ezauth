096e46368cbcca6a39ce77e707c6ba8573e1538d
mmdevelops <mmdevelops@gmail.com>
2019-03-26 14:22:46 -0700

    add nbf and exp claims

--- services/jwt.js
+++ services/jwt.js
@@ -1,11 +1,20 @@
 const secret = process.env.SECRET
 
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken'),
+	expMinutes = 60
 
 module.exports.tokenForUserAsync = function (user, cb) {
-	jwt.sign({ sub: user.id, iat: Date.now() }, secret, { algorithm: 'HS256' },
-		function (err, token) {
-			if (err) cb(err)
-			cb(null, token)
-		})
+	jwt.sign({
+		sub: user.id,
+		// Token times in seconds since epoch
+		iat: Math.floor(Date.now() / 1000),
+		nbf: Math.floor(Date.now() / 1000) - (2 * 60),
+		// Set token to expire in 1 hour
+		exp: Math.floor(Date.now() / 1000) + (expMinutes * 60),
+	},
+	secret, { algorithm: 'HS256' },
+	function (err, token) {
+		if (err) cb(err)
+		cb(null, token)
+	})
 }
