91f7b4a62a4201dc15149443f758ab2837fd048c
mmdevelops <mmdevelops@gmail.com>
2019-03-26 13:53:53 -0700

    specify jwt algorithm and require for authentication

--- services/jwt.js
+++ services/jwt.js
@@ -3,7 +3,7 @@ const secret = process.env.SECRET
 const jwt = require('jsonwebtoken')
 
 module.exports.tokenForUserAsync = function (user, cb) {
-	jwt.sign({ sub: user.id, iat: Date.now() }, secret,
+	jwt.sign({ sub: user.id, iat: Date.now() }, secret, { algorithm: 'HS256' },
 		function (err, token) {
 			if (err) cb(err)
 			cb(null, token)
